85e2ac1a5c1fcbbdf275c7fa8125242d4be4b176
DMells <dave@davemellor.com>
2018-10-08 16:49:32 +0800

    Add df slicing and save upon error

--- DM_orgtype_classifier_v9.py
+++ DM_orgtype_classifier_v9.py
@@ -11,7 +11,7 @@ import pdb
 import math
 import config
 import sys
-from urllib2 import HTTPError
+import urllib3
 
 # Define input arguments for data file and directory customisation
 def get_input_args():
@@ -28,7 +28,7 @@ def get_input_args():
 def load_df(data_dir, data_file):
     df = pd.read_csv(str(data_dir + data_file))
     df_name = str(data_file)[:-4]
-    # df = df[0:10]
+    df = df[0:50]
     return df, df_name
 
 def pre_processing(df):
@@ -118,79 +118,70 @@ def map_columns(df):
     # Create comp or not column mapping the values of comp_or_not to the
     # org_type
     df['company_or_not'] = df['org_type'].map(comp_or_not_dict)
-    org_id_dict = get_org_id(df)
-    df['obtained_id'] = df['org_string'].map(org_id_dict)
+    df = get_org_id(df)
+    # df['obtained_id'] = df['org_string'].map(org_id_dict)
     return df
 
-    # MULTIPLE STRING SEARCH - NOT WORKING - ONLY RETURNS ONE STRING
-# Function which calls to the Companies House API via chwrapper, passes in org_string column from df
-# def get_org_id(df):
-#     org_id = {}
-#     # chunk_propn = 0
-#     s = chwrapper.Search(access_token=config.api_key)
-#     # org_strings = df['org_string']
-#     org_strings = ['001 INSPIRATION LIMITED', '007 PEST CONTROL LTD']
-#     print(org_strings)
-#     # math.ceil(len(org_strings)/600)
-#     # for chunk in np.array_split(org_strings, 50):
-#     # print("Processing companies house batch of size: " + str(len(chunk)))
-#     r = s.search_companies(list(org_strings))
-#     print(r.text)
-#     comp_house_dict = r.json()
-#     # For each org_string in the sub-array of org_strings, pull org data from companies house
-#     for word in org_strings:
-#         for i in range(0, len(comp_house_dict['items']), 1):
-#             org_title = str(comp_house_dict['items'][i]['title'])
-#             basic_string_comph = org_title.replace("  ", " ")
-#             basic_string_df = word.replace("  ", " ")
-#             if basic_string_comph == basic_string_df:
-#                 print("MATCH")
-#                 org_id[word] = comp_house_dict['items'][i]['company_number']
-#                 org_id.update(org_id)
-
-#         #  # Print progress
-#         # chunk_propn += int(len(chunk))
-#         # print("Progress: " + str(chunk_propn) + " of " + str(len(org_strings)))
-
-# #         # Companies House API only allows up to 600 requests per 5 mins.
-# #         # If the batch wasn't the last batch, wait for 5 mins, then loop back to grab next chunk
-#         # if chunk_propn < len(org_strings):
-#         #     print("Sleeping as close to batch limit")  
-#         #     # time.sleep(5 * 60)
-#     print(org_id)
-#     return org_id
-
-# SINGLE STRING SEARCH
 # Function which calls to the Companies House API via chwrapper, passes in org_string column from df
 def get_org_id(df):
     s = chwrapper.Search(access_token=config.api_key)
     org_strings = df['org_string']
     org_id = {}
-    # chunk_propn = 0
+    # chunk_propn = 0    
+
     
+    def call_api(org_strings):
     # Split org_string array into multiple arrays. Length of sub-array based on max batch size of 600 
-    # rounded up. array_split doesn't have to have equal batch sizes
-    for chunk in np.array_split(org_strings, math.ceil(len(org_strings)/600), axis=0):
-        print("\nProcessing companies house batch of size: " + str(len(chunk)))
-        # For each org_string in the sub-array of org_strings, pull org data from companies house
-        for word in chunk:
-            r = s.search_companies(word)
-            comp_house_dict = r.json()
-            # r returns a nested dict with complete info on the org. Below pulls just the ID number.
-            org_id[word] = comp_house_dict['items'][0]['company_number']
-            org_id.update(org_id)
-        # Print progress
-        # chunk_propn += int(len(chunk))
-        # time.sleep(0.5)
-        # print("\nProgress: " + str(chunk_propn) + " of " + str(len(org_strings)))
+    # rounded up. array_split doesn't have to have equal batch sizes.
+        for chunk in np.array_split(org_strings, math.ceil(len(org_strings)/10), axis=0):
+            print("\nProcessing companies house batch of size: " + str(len(chunk)))
+            # For each org_string in the sub-array of org_strings, pull org data from companies house
+            for word in chunk:
+                r = s.search_companies(word)
+                comp_house_dict = r.json()
+                # r returns a nested dict with complete info on the org. Below pulls just the ID number.
+                org_id[word] = comp_house_dict['items'][0]['company_number']
+                org_id.update(org_id)
+        return org_id
 
-        # Companies House API only allows up to 600 requests per 5 mins.
-        # # If the batch wasn't the last batch, wait for 5 mins, then loop back to grab next chunk
-        # if chunk_propn < len(org_strings):
-        #     print("\nSleeping as close to batch limit")  
-        #     time.sleep(5 * 60)
+    while True:
+        try:
+            # If obtained_id already exists (i.e previous runtime crashed, but saved progress)
+            # Filter the df for only the blank obtained_id rows
+            if 'obtained_id' in df:
+            # Filter df for blank ids
+                isnull = df.obtained_id.isnull()
+                notnull = df.obtained_id.notnull()
+                empty_id_df = df[isnull]
+                filled_id_df = df[notnull]
+                org_strings = empty_id_df['org_string']
+                partial_org_id = call_api(org_strings)
+                empty_id_df['obtained_id'] = empty_id_df['org_string'].map(partial_org_id)
+                df_merged = pd.concat([filled_id_df, empty_id_df])
+                return df_merged
+            else:
+                org_id = call_api(org_strings)
+                df['obtained_id'] = df['org_string'].map(org_id)
+                return df
 
+        except:
+                df['obtained_id'] = df['org_string'].map(org_id)
+                return df
+                # Save progress after each chunk
+                save_adjusted_data(df, df_name)
+                continue
+        break
     return org_id
+             # Print progress
+            # chunk_propn += int(len(chunk))
+            # time.sleep(0.5)
+            # print("\nProgress: " + str(chunk_propn) + " of " + str(len(org_strings)))
+
+            # Companies House API only allows up to 600 requests per 5 mins.
+            # # If the batch wasn't the last batch, wait for 5 mins, then loop back to grab next chunk
+            # if chunk_propn < len(org_strings):
+            #     print("\nSleeping as close to batch limit")  
+            #     time.sleep(5 * 60)
 
 def post_processing(df, df_name):
     time.sleep(1)
@@ -199,6 +190,7 @@ def post_processing(df, df_name):
     time.sleep(0.5)
     print("\nChecking org_id data: ")
     time.sleep(0.5)
+    df['obtained_id'] = df['obtained_id'].astype(int)
     nans = lambda df: df.loc[df['obtained_id'].isnull()]
     print("\nThere are {} blank org_ids in the file.".format(len(nans(df))))
     if len(nans(df)) != 0:
@@ -211,7 +203,7 @@ def post_processing(df, df_name):
             try:
                 print("\nComparing obtained_ids to pre-analysed ids...")
                 df[id_comparison] = df[id_comparison].astype(int)
-                df['obtained_id'] = df['obtained_id'].astype(int)
+                
             except KeyError:
                 id_comparison = str(input("\nIncorrect id column name entered, try again :"))
                 continue
@@ -259,6 +251,7 @@ def test_checknoblanks():
 def test_checkconnectedtolocalhost():
     assert os.system("ping -c 1 localhost") == 0
 
+# def test_shouldsavedfwhenhttperror
 
 # ---------------------------------------------------------------
 if __name__ == '__main__':
